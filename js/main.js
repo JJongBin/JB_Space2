bc89490343d496326f2e02ec599be50c58a35d0d
JJongBin <gghkdu2@gmail.com>
2022-02-22 22:20:16 +0900

    feat: add portfolio draw to me
    - fix modal js

--- js/main.js
+++ js/main.js
@@ -1,148 +1,118 @@
-// location.reload()
-console.log("원종빈의 포트폴리오입니다.")
-// ########################################
-// vs code 타이핑
-// if (matchMedia("screen and (min-width: 1024px)").matches) {
-  
-// }
+console.log('원종빈의 포트폴리오입니다.');
 
-let typingBool = false; 
-let typingIdx = 0; 
+let typingBool = false;
+let typingIdx = 0;
 let liIndex = 0;
-const liLength = $(".typing-txt>ul>li").length;
+const typingUl = document.querySelector('.typing-txt ul');
+const liLength = typingUl.children.length;
+const typingTarget = document.querySelector('.typing ul');
 
-// 타이핑될 텍스트를 가져온다 
-let typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
-let tyInt
-typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
-if(typingBool==false){ // 타이핑이 진행되지 않았다면 
-    typingBool=true; 
-    let tyInt = setInterval(typing,60); // 반복동작 
-} 
-     
-function typing(){ 
-  $(".typing ul li").removeClass("on");
-   $(".typing ul li").eq(liIndex).addClass("on");
-  if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
-     $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
-     typingIdx++; 
-   } else{ if(liIndex<liLength-1){
-     //다음문장으로  가기위해 인덱스를 1증가
-       liIndex++; 
-     //다음문장을 타이핑하기위한 셋팅
-        typingIdx=0;
-        typingBool = false; 
-        typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
-       
-     //다음문장 타이핑전 1초 쉰다
-         clearInterval(tyInt);
-          //타이핑종료
-     
-         setTimeout(function(){
-           //1초후에 다시 타이핑 반복 시작
-           tyInt = setInterval(typing,50);
-         },500);
-        } else if(liIndex==liLength-1){
-          
-         //마지막 문장까지 써지면 반복종료
-           clearInterval(tyInt);
-        }
-    } 
-}  
+let typingTxt;
+let tyInt;
 
+if (typingBool == false) {
+  typingBool = true;
+  tyInt = setInterval(typing, 60);
+}
 
-// ##################################
-// introduce 애니메이션
-// https://blog.naver.com/sungyuna89/222180198536
+function typing() {
+  typingTxt = typingUl.children[liIndex].innerText;
+  typingTxt = typingTxt.split('');
 
-const intro = document.querySelector(".whoami .inner .head"),
-docElem = document.documentElement
+  for (let i = 0; i < liIndex; i++) {
+    typingTarget.children[i].classList.remove('on');
+  }
+  typingTarget.children[liIndex].classList.add('on');
+
+  if (typingIdx < typingTxt.length) {
+    typingTarget.children[liIndex].append(typingTxt[typingIdx]); // 한글자씩 이어준다.
+    typingIdx++;
+  } else {
+    if (liIndex < liLength - 1) {
+      liIndex++;
+      typingIdx = 0;
+      typingBool = false;
+      typingTxt = typingTarget.children[liIndex].innerText;
+
+      clearInterval(tyInt);
+
+      setTimeout(function () {
+        tyInt = setInterval(typing, 60);
+      }, 500);
+    } else if (liIndex == liLength - 1) {
+      clearInterval(tyInt);
+    }
+  }
+}
+
+const intro = document.querySelector('.whoami .inner .head'),
+  docElem = document.documentElement;
 let offset, // 기준(여기까지 오면 띄움)
-scrollPos,  //진행한 스크롤양
-docHeight; //전체높이
-const introLogo = document.querySelector(".introduce1 .logo");
-const introText = document.querySelector(".introduce1 .text");
-const introSkills = document.querySelector(".introduce2 .skills");
+  scrollPos, //진행한 스크롤양
+  docHeight; //전체높이
+const introLogo = document.querySelector('.introduce1 .logo');
+const introText = document.querySelector('.introduce1 .text');
+const introSkills = document.querySelector('.introduce2 .skills');
 const introSkillsChild = introSkills.children;
 
-// console.log(introSkills.children)
-// console.log(introSkills.children.length)
 // 높이 계산하기
 docHeight = Math.max(docElem.offsetHeight, docElem.scrollHeight);
 
-scrollPos = docElem.scrollTop ;
+scrollPos = docElem.scrollTop;
 
-window.addEventListener('scroll', _.throttle(function() {
-  console.log(scrollPos)
-},1000));
+window.addEventListener(
+  'scroll',
+  _.throttle(function () {}, 1000)
+);
 
-
-
-const targetWho = document.getElementById("whoami_move");
+const targetWho = document.getElementById('whoami_move');
 const targetWhoTop = targetWho.getBoundingClientRect();
-offset = targetWhoTop.top + scrollPos + 100;;
+offset = targetWhoTop.top + scrollPos + 100;
 
-const goTop = document.querySelector(".go_top");
-window.addEventListener('scroll', function() {
+const goTop = document.querySelector('.go_top');
+window.addEventListener('scroll', function () {
   scrollPos = docElem.scrollTop;
-  
+
   if (scrollPos + window.innerHeight > offset) {
     intro.classList.add('intro_ani');
     intro.classList.remove('invisible');
 
-
-    setTimeout(function() {
+    setTimeout(function () {
       introLogo.classList.remove('invisible');
       introLogo.classList.add('intro_ani');
       introText.classList.remove('invisible');
       introText.classList.add('intro_ani');
 
-      setTimeout(function() {
-        introSkills.classList.remove('invisible')
-        introSkills.classList.add('intro_ani')
+      setTimeout(function () {
+        introSkills.classList.remove('invisible');
+        introSkills.classList.add('intro_ani');
 
-        for(let i = 0; i<introSkillsChild.length; i++){
-          const fill = introSkillsChild[i].querySelector('.fill')
-          const fill_percent = fill.querySelector('p')
-          // console.log(fill_percent)
-          // console.log(fill_name)
-          if (intro.className.includes('invisible') == true){
-            continue
+        for (let i = 0; i < introSkillsChild.length; i++) {
+          const fill = introSkillsChild[i].querySelector('.fill');
+          const fill_percent = fill.querySelector('p');
+
+          if (intro.className.includes('invisible') == true) {
+            continue;
           }
-          const percentFill = setTimeout(function() {
-            fill_name = fill.className
-            // //이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??
-            // //이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??
-            // //이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??
-            // if (intro.className.includes('invisible') == true){
-            //   clearTimeout(percentFill)
-            //   console.log('stop!!!')
-            // } //이거 왜 안되냐??
-            // //이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??
-            // //이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??
-            // //이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??//이거 왜 안되냐??
-            if(fill_name.includes('50')){
+          const percentFill = setTimeout(function () {
+            fill_name = fill.className;
+            if (fill_name.includes('50')) {
               fill.classList.add('percent50_ani');
-            }else if(fill_name.includes('60')){
+            } else if (fill_name.includes('60')) {
               fill.classList.add('percent60_ani');
-            }else if(fill_name.includes('70')){
+            } else if (fill_name.includes('70')) {
               fill.classList.add('percent70_ani');
-            }else if(fill_name.includes('80')){
+            } else if (fill_name.includes('80')) {
               fill.classList.add('percent80_ani');
             }
-          }, 400*(i+1));
-          setTimeout(function() {
-            fill_percent.classList.remove('invisible')
-          }, 400*(i+1)+300);
-        };
+          }, 400 * (i + 1));
+          setTimeout(function () {
+            fill_percent.classList.remove('invisible');
+          }, 400 * (i + 1) + 300);
+        }
       }, 400);
     }, 300);
-  }
-  // if (scrollPos < offset){  // 애니매이션 초기화
-  
-  else if(scrollPos + window.innerHeight < offset){
-    
-
+  } else if (scrollPos + window.innerHeight < offset) {
     intro.classList.add('invisible');
     intro.classList.remove('intro_ani');
     introLogo.classList.add('invisible');
@@ -150,57 +120,50 @@ window.addEventListener('scroll', function() {
     introText.classList.add('invisible');
     introText.classList.remove('intro_ani');
 
-    introSkills.classList.add('invisible')
-    introSkills.classList.remove('intro_ani')
+    introSkills.classList.add('invisible');
+    introSkills.classList.remove('intro_ani');
 
-    for(let i = 0; i<introSkillsChild.length; i++){
-      const fill = introSkillsChild[i].querySelector('.fill')
-      const fill_percent = fill.querySelector('p')
-      // console.log(fill_percent)
-      // console.log(fill_name)
-      
-      fill_name = fill.className
-      if(fill_name.includes('50')){
+    for (let i = 0; i < introSkillsChild.length; i++) {
+      const fill = introSkillsChild[i].querySelector('.fill');
+      const fill_percent = fill.querySelector('p');
+
+      fill_name = fill.className;
+      if (fill_name.includes('50')) {
         fill.classList.remove('percent50_ani');
-      }else if(fill_name.includes('60')){
+      } else if (fill_name.includes('60')) {
         fill.classList.remove('percent60_ani');
-      }else if(fill_name.includes('70')){
+      } else if (fill_name.includes('70')) {
         fill.classList.remove('percent70_ani');
-      }else if(fill_name.includes('80')){
+      } else if (fill_name.includes('80')) {
         fill.classList.remove('percent80_ani');
       }
       fill_percent.classList.add('invisible');
-    };
-  };
-  if (scrollPos < 100){
+    }
+  }
+  if (scrollPos < 100) {
     goTop.classList.remove('intro_ani');
     goTop.classList.add('none_ani');
-    setTimeout(function() {
+    setTimeout(function () {
       goTop.classList.add('invisible');
     }, 700);
   } else {
-    goTop.classList.add("intro_ani");
+    goTop.classList.add('intro_ani');
     goTop.classList.remove('invisible');
     goTop.classList.remove('none_ani');
   }
 });
-  
 
-// #############################
 // portfolio
 
-const portHead = document.querySelector(".portfolio .head");
-const portContents = document.querySelector(".portfolio .contents");
+const portHead = document.querySelector('.portfolio .head');
+const portContents = document.querySelector('.portfolio .contents');
 
-
-const targetPortfolio = document.getElementById("portfolio_move");
+const targetPortfolio = document.getElementById('portfolio_move');
 let targetTopPortfolio = targetPortfolio.getBoundingClientRect();
-// offset2 = targetTopPortfolio.top + scrollPos - window.innerHeight + 200;
 let offset2 = offset + targetWhoTop.height;
-let offset3
+let offset3;
 
-
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   scrollPos = docElem.scrollTop;
   if (scrollPos + window.innerHeight > offset2) {
     portHead.classList.remove('invisible');
@@ -209,13 +172,11 @@ window.addEventListener('scroll', function() {
     targetTopPortfolio = targetPortfolio.getBoundingClientRect();
     offset3 = offset2 + targetPortfolio.offsetHeight;
 
-    setTimeout(function() {
+    setTimeout(function () {
       portContents.classList.remove('invisible');
       portContents.classList.add('intro_ani2');
     }, 300);
-    
-  }
-  else if(scrollPos + window.innerHeight < offset2){
+  } else if (scrollPos + window.innerHeight < offset2) {
     portHead.classList.add('invisible');
     portHead.classList.remove('intro_ani');
     portContents.classList.add('invisible');
@@ -223,31 +184,22 @@ window.addEventListener('scroll', function() {
   }
 });
 
-
-// #############################
 // certificate
 
-const cerHead = document.querySelector(".certificate  .head");
-const cerContents = document.querySelector(".certificate .contents");
+const cerHead = document.querySelector('.certificate  .head');
+const cerContents = document.querySelector('.certificate .contents');
 
-
-// let targetCertificate = document.getElementById("certificate_move");
-// let targetTopCertificate = targetCertificate.getBoundingClientRect();
-// offset3 = targetTopCertificate.top + scrollPos - window.innerHeight + 200;
-// let offset3 = offset2 + targetPortfolio.offsetHeight;
-
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   scrollPos = docElem.scrollTop;
   if (scrollPos + window.innerHeight > offset3) {
     cerHead.classList.remove('invisible');
     cerHead.classList.add('intro_ani');
 
-    setTimeout(function() {
+    setTimeout(function () {
       cerContents.classList.remove('invisible');
       cerContents.classList.add('intro_ani2');
     }, 300);
-  }
-  else if(scrollPos + window.innerHeight < offset3){
+  } else if (scrollPos + window.innerHeight < offset3) {
     cerHead.classList.add('invisible');
     cerHead.classList.remove('intro_ani');
     cerContents.classList.add('invisible');
@@ -255,72 +207,52 @@ window.addEventListener('scroll', function() {
   }
 });
 
-$("#go_top").click(function() {
-  $('html, body').animate({ scrollTop:$(docElem).offset().top}, 300);
-  // setTimeout(function() {
-  //   scrollPos = docElem.scrollTop;
-  //   if (scrollPos < 50){
-  //     intro.classList.add('invisible');
-  //     intro.classList.remove('intro_ani');
-  //     introLogo.classList.add('invisible');
-  //     introLogo.classList.remove('intro_ani');
-  //     introText.classList.add('invisible');
-  //     introText.classList.remove('intro_ani');
-  //     portHead.classList.add('invisible');
-  //     portHead.classList.remove('intro_ani');
-  //     portContents.classList.add('invisible');
-  //     portContents.classList.remove('intro_ani2');
-  //     cerHead.classList.add('invisible');
-  //     cerHead.classList.remove('intro_ani');
-  //     cerContents.classList.add('invisible');
-  //     cerContents.classList.remove('intro_ani2');
-  //   };
-  // }, 500);
-});
-$("#go_whoami").click(function() {
-  $('html, body').animate({ scrollTop:$("#whoami_move").offset().top-120}, 500);
-});
-$("#go_portfolio").click(function() {
-  $('html, body').animate({ scrollTop:$("#portfolio_move").offset().top-120}, 500);
-});
-$("#go_certificate").click(function() {
-  $('html, body').animate({ scrollTop:$("#certificate_move").offset().top-120}, 500);
+document.querySelector('#go_top').addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
 });
 
+document.querySelector('#go_whoami').addEventListener('click', () => {
+  window.scroll({
+    top: document.querySelector('#whoami_move').getBoundingClientRect().top - 120,
+    left: 0,
+    behavior: 'smooth',
+  });
+});
 
+document.querySelector('#go_portfolio').addEventListener('click', () => {
+  window.scroll({
+    top: document.querySelector('#portfolio_move').getBoundingClientRect().top - 120,
+    left: 0,
+    behavior: 'smooth',
+  });
+});
+
+document.querySelector('#go_certificate').addEventListener('click', () => {
+  window.scroll({
+    top: document.querySelector('#certificate_move').getBoundingClientRect().top - 120,
+    left: 0,
+    behavior: 'smooth',
+  });
+});
 
 // MODAL
-const open1 = () => {
-  document.querySelector(".modal1").classList.remove("invisible");
-  document.querySelector('body').classList.add('modal-open');
-}
-const close1 = () => {
-  document.querySelector(".modal1").classList.add("invisible");
-  document.querySelector('body').classList.remove('modal-open');
-}
 
-// const open2 = () => {
-//   document.querySelector(".modal2").classList.remove("invisible");
-//   document.querySelector('body').classList.add('modal-open');
-// }
-// const close2 = () => {
-//   document.querySelector(".modal2").classList.add("invisible");
-//   document.querySelector('body').classList.remove('modal-open');
-// }
+const certificate = document.querySelector('.certificate .contents');
 
-const open3 = () => {
-  document.querySelector(".modal3").classList.remove("invisible");
-  document.querySelector('body').classList.add('modal-open');
-}
-const close3 = () => {
-  document.querySelector(".modal3").classList.add("invisible");
-  document.querySelector('body').classList.remove('modal-open');
-}
+const handleModal = e => {
+  if (e.target.dataset.id) {
+    document.querySelector(`.modal${e.target.dataset.id}`).classList.remove('invisible');
+    const bg = document.querySelector(`.modal_bg${e.target.dataset.id}`);
 
-document.querySelector(".certificate .contents .m1").addEventListener("click", open1);
-// document.querySelector(".certificate .contents .m2").addEventListener("click", open2);
-document.querySelector(".certificate .contents .m3").addEventListener("click", open3);
-// document.querySelector(".closeBtn").addEventListener("click", close);
-document.querySelector(".modal_bg1").addEventListener("click", close1);
-// document.querySelector(".modal_bg2").addEventListener("click", close2);
-document.querySelector(".modal_bg3").addEventListener("click", close3);
+    const closeModalEvent = bg.addEventListener('click', e => {
+      document.querySelector(`.modal${e.target.dataset.id}`).classList.add('invisible');
+      removeEventListener('click', closeModalEvent);
+    });
+  }
+};
+
+certificate.addEventListener('click', handleModal);
